5027a1eb087c37fbc393aa54c3de571fc166f967
skinbark <dgbrown3070@gmail.com>
2013-12-20 11:23:46 -0600

    Update device.mk

--- device.mk
+++ device.mk
@@ -73,6 +73,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml
+#    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.ap.interface=wl0.1
